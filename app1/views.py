b6d8af4d58d236da83f1dce7e08039ab263c2408
mohamedalijas <alijasmohammed@gmail.com>
2025-03-14 11:45:38 +0530

    font commit 2

--- app1/views.py
+++ app1/views.py
@@ -191,7 +191,7 @@ from django.conf import settings
 
 # Paths
 ID_CARD_SAVE_PATH = os.path.join(settings.BASE_DIR, 'app1', 'static', 'app1', 'generated_id_cards')
-FONT_PATH = os.path.join(settings.BASE_DIR, 'app1', 'static', 'app1', 'fonts', 'RedHatDisplay-Bold')
+FONT_PATH = os.path.join(settings.BASE_DIR, 'app1', 'static', 'app1', 'fonts', 'RedHatDisplay-Bold.ttf')
 
 TEMPLATE_PATHS = {
     'type1': 'https://piqapi.purpleiq.ai/uploads/EmployeeImage/bahrainsteelCont.jpeg',
